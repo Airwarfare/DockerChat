805fd93abf1dec8dd2c975656b0c1473f59407d0
Airwarfare <jordanscast@gmail.com>
2019-03-15 11:54:42 -0500

    Added Redis container, build file and connection

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 @ECHO OFF
 FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker stop %%i
+docker run --net=host -p 0.0.0.0:6379:6379 -d redis
+::timeout 3
 docker build -t dockerchat .
-docker run -p 8080:8080 -d dockerchat
+docker run --net=host -p 8080:8080 -d dockerchat 
+
--- index.js
+++ index.js
@@ -8,8 +8,27 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+//Redis
+var redis = require("redis"),
+    client = redis.createClient()
+
+
+//On Error, log it (For Redis only)
+client.on("error", function(err) {
+  console.log("Error " + err);
+});
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('Test build .bat');
+  res.send('Test build .bat'); //Send the html file later
+});
+
+
+client.on('connect', function() {
+  console.log('Redis client connected'); //Just for sanity sake
 });
 
 app.listen(PORT, HOST);
