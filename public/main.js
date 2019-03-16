0a32e27c7d20d86d7aa00bb72c7701ca3412c7dc
Airwarfare <jordanscast@gmail.com>
2019-03-16 06:44:13 -0500

    Added input, css, and some formating, working on the message readablity next, the main functionality is done

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 console.log('Hello Docker!');
 var Username = ""; //Current Username, TODO: check the server for currently taken usernames, (Maybe add an account system with and api?)
 do{
-    Username = prompt("Please enter your username");
+    Username = prompt("Please enter your username", "Airwarfare");
 }while(Username == null || Username == "" ); //Make sure the user give us an input
 
 var socket = io();
@@ -15,5 +15,22 @@ socket.on('clientmessage', function(msg) {
 
 //Adds a message to the list
 function AddMessage(msg) {
-    $('#chatmessagebox').append($('<li>').text(msg)); //Add message to support bottom to top
+    $('#chatmessagebox').append($('<li style="padding: 2px;font-size: 20px;">').text(msg)); //Add message to support bottom to top
 }
+
+//https://stackoverflow.com/a/9964945
+//Thanks to stack overflow for this one
+$.fn.enterKey = function (fnc) {
+    return this.each(function () {
+        $(this).keypress(function (ev) {
+            var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+            if (keycode == '13') {
+                fnc.call(this, ev);
+            }
+        })
+    })
+}
+
+$("#messageinput").enterKey(function() { //Get enter key on the message input
+    socket.emit('message', Username + ': ' + $("#messageinput").val()); //send it to the server
+})
